4750a9b8ac046082e0fe96e42f1a8ede160b22dc
Divorius <iurysilvadealmeida2@hotmail.com>
2023-01-05 15:54:31 -0400

    Commit

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -3,13 +3,25 @@ public class SmartTv {
     int canal=1;
     int volume = 25;
 
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        volume++;
+    }
+    public void diminuirCanal(){
+        volume--;
+    }
+    public void aumentarVolume(){
+        volume++;
+    }
+    public void diminuirVolume(){
+        volume--;
+    }
     public void ligar (){
         ligada=true;
     }
     public void desligar (){
         ligada=false;
     }
-
-
-    
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -3,11 +3,21 @@ public class Usuario {
         
         SmartTv smartTv = new SmartTv();
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+
+
         System.out.println("A tv ligada? " + smartTv.ligada);
         System.out.println("Canal Atual: " +smartTv.canal);
         System.out.println("Volume Atual: " +smartTv.volume);
 
         smartTv.ligar ();
-        System.out.println("Novo status: A tv ligada?" + smartTv.ligada);
+        System.out.println("Novo status: A tv ligada? " + smartTv.ligada);
+        smartTv.desligar ();
+        System.out.println("Novo status: A tv ligada? " + smartTv.ligada);
     }
 }
