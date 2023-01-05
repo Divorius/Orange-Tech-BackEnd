b5ef467e7a587637843c7dbdebdd3eb89dbe5bca
Divorius <iurysilvadealmeida2@hotmail.com>
2023-01-05 13:31:47 -0400

    Commit

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposVariaveis {
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int numeroUm = 1;
+        int numeroDois = 2;
+
+        System.out.println
+        ("faça o calculo das duas variavel: " ); 
+
+         System.out.println
+         (numeroUm + numeroDois);
+
+    }
+}
