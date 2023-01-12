6aa7a8dab2a1917c9cb2df54f306b8dd6a4bfe3b
Divorius <iurysilvadealmeida2@hotmail.com>
2023-01-11 23:12:14 -0400

    Fazendo Exercicios

---
+++ Desafio-de-codigo-2/src/SalvandoMusicas.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+    
+public class SalvandoMusicas {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String arquivoDoPc = leitor.next();
+        
+
+
+    }
+}
