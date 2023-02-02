cbcdfdbdcb217a4a4e44da26fba16dc61d233c39
Iury Silva de Almeida <102688380+IuryAlmeiida@users.noreply.github.com>
2023-02-02 14:13:32 -0400

    att

--- desafio-orangetech-designpatterns-main/src/main/java/com/designpatterns/service/ViaCepService.java
+++ desafio-orangetech-designpatterns-main/src/main/java/com/designpatterns/service/ViaCepService.java
@@ -6,15 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.designpatterns.model.Endereco;
 
-/**
- * Client HTTP, criado via OpenFeign, para o consumo da API do ViaCEP.
- * 
- * @see <a href="https://spring.io/projects/spring-cloud-openfeign">Spring Cloud OpenFeing</a>
- * @see <a href="https://viacep.com.br">ViaCEP</a> 
- * 
- * @author joanaleoni
- *
- */
+
 @FeignClient(name="viacep", url="https://viacep.com.br/ws")
 public interface ViaCepService {
 	
